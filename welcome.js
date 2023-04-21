cedfa27f6ba28f6acecd93711d2be2e6e92bf6fe
Neurs <31346263+Neurs12@users.noreply.github.com>
2023-04-21 23:20:20 +0700

    Delete welcome.js

--- welcome.js
+++
@@ -1,5 +0,0 @@
-setTimeout(() => {document.getElementById("class-logo").classList.add("fade-ani-in");}, 800);
-setTimeout(() => {document.getElementById("school-logo").classList.add("fade-ani-in");}, 1200);
-setTimeout(() => {document.getElementById("title").classList.add("fade-ani-in");}, 1500);
-setTimeout(() => {document.getElementById("description").classList.add("fade-ani-in");}, 1700);
-setTimeout(() => {document.getElementById("next").classList.add("next-in")}, 3000);
